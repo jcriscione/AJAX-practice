9f72e7516359a6b95cfc3de29d47e08c81d2debc
SyntacticX <rmm54@njit.edu>
2015-10-26 21:32:24 -0400

    commit 4

--- xTestJS.js
+++ xTestJS.js
@@ -3,14 +3,18 @@ function processForm() {
     qObj["Question"] = document.getElementById("question").value;
     qObj["Answer"] = document.getElementById("answer").value;
     */
-    
     var xhr = new XMLHttpRequest();
+    console.log("Test 1: " + xhr.readyState);
     xhr.onreadystatechange = function (){
       if (xhr.readyState === 4){
+        console.log("Test 2: " + xhr.readyState);
         document.getElementById('response').innerHTML = xhr.responseText;
       }
     };
+    console.log("Test 3: " + xhr.readyState);
     xhr.open('GET', 'xTestPHP.php');
+    console.log("Test 4: " + xhr.readyState);
     xhr.send();
+    console.log("Test 5: " + xhr.readyState);
 
 }
